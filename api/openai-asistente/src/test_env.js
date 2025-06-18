f8d501e0e7ee19cc7ab847920f0e3da4f3f074de
LuisSantaoca <santaoca@gmail.com>
2025-06-17 20:21:28 -0600

    cambios test .env ruta corregida2

--- api/openai-asistente/src/test_env.js
+++ api/openai-asistente/src/test_env.js
@@ -1,5 +1,5 @@
 const OpenAI = require('openai');
-require('dotenv').config(); // carga automáticamente .env en la raíz
+require('dotenv').config({ path: '/var/www/agentes/config/.env' });
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
