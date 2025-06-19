9061dbe961e29b60ad9f9fa4536c21dfcc93162d
LuisSantaoca <santaoca@gmail.com>
2025-06-18 22:57:38 -0600

    cambios todos simplificado en interactivo router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,23 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import TenantIndex from './pages/TenantIndex'
-import ProtectedLanding from './pages/PruebasAcro/ProtectedLanding'
-import OpenAIChatPage from './pages/tenants/kokitos/OpenAIChatPage'
+// import TenantIndex from './pages/TenantIndex'
+// import ProtectedLanding from './pages/PruebasAcro/ProtectedLanding'
+// import OpenAIChatPage from './pages/tenants/kokitos/OpenAIChatPage'
 import LandingChatInteractivoPage from './pages/LandingChatInteractivoPage'
-import LandingInstitucional from './pages/LandingInstitucional/ui/LandingInstitucional' // ← Importación añadida
+import LandingInstitucional from './pages/LandingInstitucional/ui/LandingInstitucional'
 
 // ====== ROUTER PRINCIPAL ======
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingInstitucional />} /> {/* ← Cambiado claramente al índice */}
-        <Route path="/landing" element={<TenantIndex />} />
-        <Route path="/kokitos/openai-chat" element={<OpenAIChatPage />} />
+        <Route path="/" element={<LandingInstitucional />} />
+        {/* <Route path="/landing" element={<TenantIndex />} /> */}
+        {/* <Route path="/kokitos/openai-chat" element={<OpenAIChatPage />} /> */}
         <Route path="/landing/chat-interactivo" element={<LandingChatInteractivoPage />} />
-        <Route path="/pruebas" element={<ProtectedLanding />} /> {/* ← Mantenemos ruta alternativa si es útil */}
-        <Route path="*" element={<TenantIndex />} />
+        {/* <Route path="/pruebas" element={<ProtectedLanding />} /> */}
+        {/* <Route path="*" element={<TenantIndex />} /> */}
       </Routes>
     </Router>
   )
 }
-
